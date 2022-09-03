5f05f7b4141001ac46a530cc0f38a4fa78150df7
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-09-03 20:58:43 +0600

    modal added

--- index.js
+++ index.js
@@ -27,42 +27,82 @@ const loadAllNews = (news_id) => {
 
 const displayNews = (datas) => {
     //  console.log(datas);
-    console.log(datas.length);
+    // console.log(datas.length);
     const newsAlert = document.getElementById('newsAlert')
-    if(datas.length === 0){
+    if (datas.length === 0) {
         newsAlert.classList.remove("d-none")
     }
-    else{
-        newsAlert.classList.add("d-none")  
+    else {
+        newsAlert.classList.add("d-none")
     }
     const newsContainer = document.getElementById("newsContainer");
     newsContainer.innerHTML = ``;
     datas.forEach(data => {
         // console.log(data);
-         
+
         const newsDiv = document.createElement("div");
         newsDiv.classList.add("card");
         newsDiv.innerHTML = `
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="${data.image_url}" class="img-fluid rounded-start" alt="...">
+                        <img src="${data.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title">${data.author.name}</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural
-                                lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <h5 class="card-title">${data.title}</h5>
+                            <p class="card-text">${data.details.slice(0, 400)} </p>
+                            <div class="d-flex align-items-center justify-content-around mt-4">
+
+                                <div class="d-flex align-items-center w-50 " >
+                                    <img class="blogerImg" src="${data.author.img}" alt="">
+                                    <div>
+                                        <h4>${data.author.name}</h4>
+                                        <p class="mb-0">${data.author.published_date}</p>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center">
+                                     <i class="fa-regular fa-eye"></i>
+                                     <p class= "mb-0"> ${data.total_view} </p>
+                                </div>
+                                <div>
+                               <button onclick="getDetails('${data._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#blogDetailModal">Details</button>
+                            </div>  
                         </div>
                     </div>
                 </div>
-       
        `
         newsContainer.appendChild(newsDiv);
 
     });
 }
+
+const getDetails = (detailsId) =>{
+    // console.log('button clicked');
+    // console.log(detailsId);
+    const url = `https://openapi.programming-hero.com/api/news/${detailsId}`
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+}
+
+const displayDetails = (datas) => {
+    // console.log(datas);
+    datas.forEach(data => {
+        // console.log(data);
+        const blogDetailModalLabel = document.getElementById('blogDetailModalLabel')
+        blogDetailModalLabel.innerText = data.title;
+        const blogDetailBody = document.getElementById('blogDetailBody');
+        blogDetailBody.innerHTML = `
+             <img class="blogerImg" src="${data.author.img}" alt="">
+            <h4> Author : ${data.author.name}</h4>
+            <p class="mb-0"> Published_date : ${data.author.published_date}</p>
+
+        `
+    });
+
+}
+
 
 
 loadCategory();
